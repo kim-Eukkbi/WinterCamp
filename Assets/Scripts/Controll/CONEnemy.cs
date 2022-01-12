6c94a1298890fd5524118dfb17ce3f54d5b0b333
Eukkbi <smbaekr56@naver.com>
2022-01-12 16:37:33 +0900

    스킬 바 구현

--- Assets/Scripts/Controll/CONEnemy.cs
+++ Assets/Scripts/Controll/CONEnemy.cs
@@ -121,6 +121,9 @@ public class CONEnemy : CONCharacter
     private void Die()
     {
         base.SetActive(false);
-        StopCoroutine(freezeCo);
+        if(freezeCo != null)
+        {
+           StopCoroutine(freezeCo);
+        } 
     }
 }
